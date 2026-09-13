0e295f80abdc5981dffe98c8568122bf86e21cd7
wishme10 <mehwishsiddiquiofficialgmail.com>
2026-09-14 03:19:55 +0500

    python practice

---
+++ practice.py
@@ -0,0 +1,12 @@
+print("Hello, World!")  
+age=20
+price=19.99
+print (age, price, sep="\n")
+first_name= "john"
+is_online = False
+first_name = "john"
+last_name = "smith"
+print (first_name , last_name)
+print (f"we checked in a patient named {first_name} {last_name}")
+print (f"he is {age} years old")
+print ("he is a new patient")
