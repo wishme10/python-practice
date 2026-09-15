499dc7de18af28df51776f5b08b16eb2143e9688
wishme10 <mehwishsiddiquiofficialgmail.com>
2026-09-16 03:41:59 +0500

    practice.py of arithmetic operator,whileloop, operator precedence, if else statement

--- practice.py
+++ practice.py
@@ -29,3 +29,94 @@ course = "genz are cockcroaches"
 print (course.replace("genz", "millenials"))
 print (course.find("gen"))
 print ("croaches" in course)
+print (3 + 5)
+print (3 - 5)
+print (3/2)
+print (3//2)
+print (3 % 2)
+print (3 ** 2)
+x = 3
+x = x + 3
+print (x)
+x = 6
+x += 2
+print (x)
+x -= 2
+print (x)
+x %= 2
+print (x)
+x **= 2
+print (x)
+x = (3 + 2) * 4
+print (x)
+x = 10 > 9
+print(x)
+x = 10 < 9
+print (x)
+x = 10 >= 9
+print (x)
+x = 10 != 9
+print (x)
+x = 10 ==9
+print (x)
+price = 25
+print( price >10)
+print (price >10 and price <40)
+print (price >30 or price <40)
+print ( not price >30)
+temperature = 35
+if temperature >30: 
+    print("its a hot day")
+    print ("drink plenty of water")
+temperature = 25
+if temperature >30: 
+    print("its a hot day")
+    print ("drink plenty of water")
+print ("done")
+temperature = 24
+if temperature >30: 
+    print("its a hot day")
+    print ("drink plenty of water")
+elif temperature >20: 
+     print ("it's a nice day")
+elif temperature >10: 
+     print("it's a bit cold")
+else:
+     print("its cold day")
+print("done")
+weight= float(input ("what is your weight? "))
+unit=input("(K)g or (L)bs: " ).upper()
+if unit == "(L)bs" or unit == "L":
+     conversion = weight * 0.453592 
+     print(f"coversion in Kilograms = {conversion}" )
+elif unit == "(K)g" or unit == "K": 
+     conversion = weight / 0.453592
+     print(f"conversin in Pounds = {conversion}")
+else:
+     print("write (K)g or (L)bs")
+
+
+
+age = int(input("type your age: "))
+if age <= 25: 
+    print("you are young as your age is " + str(age) ) 
+elif age > 25  and age <= 50: 
+    print(f"your are mature as your age is {age}")
+elif age > 50: 
+    print ("you are old")
+else:
+    print("please enter correct age")
+
+a = 2
+while a <= 50:
+    print(a)
+    a += 2
+a = 3
+while a <= 15:
+     print(a * "*")
+     a += 1
+
+
+
+
+
